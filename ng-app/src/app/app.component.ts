a72c11be3f9adcb5610aa1bb53626c6e816eedf3
nuki_pon <tnuki10@gmail.com>
2017-04-25 21:59:54 +0900

    画面が出るところまで対応

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+export class Instance {
+  instance: string;
+  zone: string;
+  status: string;
+  ip: string;
+}
+
+const InstanceList: Instance[] = [
+  {
+    instance: 'ins1',
+    zone: 'zone1',
+    status: 'running',
+    ip: '1.0.0.0'
+  },
+  {
+    instance: 'ins2',
+    zone: 'zone2',
+    status: 'stop',
+    ip: '1.0.0.1'
+  }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  project = 'app-works!';
+  instanceList = InstanceList;
+  instance = this.instanceList[0];
+
+  instanceRefresh(): void{
+    alert('refresh');
+  }
+  startIns(ins: Instance): void {
+    this.instance = ins;
+    alert('start ' + this.instance.instance);
+  }
+  stopIns(ins: Instance): void {
+    this.instance = ins;
+    alert('stop ' + this.instance.instance);
+  }
 }
